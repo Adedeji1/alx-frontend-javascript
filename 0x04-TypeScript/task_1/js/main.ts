0528b0147f3baef721911600fabec8d0eff00a43
Adedeji1 <afolabiadedeji110@yahoo.com>
2025-11-07 13:08:47 +0100

    checker

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,50 +1,3 @@
-// task_1/js/main.ts
-
-interface Teacher {
-  readonly firstName: string;
-  readonly lastName: string;
-  fullTimeEmployee: boolean;
-  yearsOfExperience?: number;
-  location: string;
-  [key: string]: any;
-}
-
-interface Director extends Teacher {
-  numberOfReports: number;
-}
-
-const teacher3: Teacher = {
-  firstName: 'Adedeji',
-  fullTimeEmployee: false,
-  lastName: 'Afolabi',
-  location: 'Ibadan',
-  contract: false,
-};
-
-console.log(teacher3);
-
-const director1: Director = {
-  firstName: 'Adekunte',
-  lastName: 'Tolulope',
-  location: 'Abuja',
-  fullTimeEmployee: true,
-  numberOfReports: 17,
-};
-
-console.log('Director:', director1);
-
-interface printTeacherFunction {
-  (firstName: string, lastName: string): string;
-}
-
-function printTeacher({ firstName, lastName }: { firstName: string; lastName: string }): string {
-  return `${firstName}. ${lastName}`;
-}
-
-console.log(printTeacher({ firstName: "Adekunte", lastName: "Tolu" }));
-
-
-// Interface describing the class instance
 interface StudentClassInterface {
   workOnHomework(): string;
   displayName(): string;
@@ -54,9 +7,14 @@ interface StudentConstructor {
   new (firstName: string, lastName: string): StudentClassInterface;
 }
 
+class StudentClass {
+  firstName: string;
+  lastName: string;
 
-class StudentClass implements StudentClassInterface {
-  constructor(public firstName: string, public lastName: string) {}
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
 
   workOnHomework(): string {
     return "Currently working";
@@ -66,7 +24,3 @@ class StudentClass implements StudentClassInterface {
     return this.firstName;
   }
 }
-
-const student1 = new StudentClass("David", "Ola");
-console.log(student1.displayName());
-console.log(student1.workOnHomework());
